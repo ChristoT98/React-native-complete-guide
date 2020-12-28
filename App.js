fcdc43d2dfae78fa2ec40391830ad85af392801e
ChristoT98 <christolakshan@gmail.com>
2020-12-28 10:35:46 +0530

    Working with core components

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Starting of udemy complete guide app</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View>
+
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({ });
