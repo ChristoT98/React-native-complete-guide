a679a807297c1e2debbba6d257916ddb442de5f0
ChristoT98 <christolakshan@gmail.com>
2020-12-28 10:51:48 +0530

    Getting started with styles

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View>
-      <View>
-
+      <View style={{padding: 50}}>
+          <TextInput placeholder="Add course goal" style={{ borderColor: 'black', borderWidth: 1}} />
+          <Button title="ADD" />
       </View>
       <View>
 
